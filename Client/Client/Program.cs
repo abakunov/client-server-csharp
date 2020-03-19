5e2cc1906e2c49a790f152cffeb7dc8c6ecdb2af
Kirill Abakunov <abakunov@MacBook-Pro-Kirill.local>
2020-03-19 16:54:33 +0300

    Minimal working client and server parts

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
-namespace Client
+class MyTCPClient
 {
-    class MainClass
+    static void Main()
     {
-        public static void Main(string[] args)
+        do
         {
-            Console.WriteLine("Hello World!");
+            string message = Console.ReadLine();
+            string request = SendRequestAndRecieveAnswer
+                ("127.0.0.1", 13000, message);
+            Console.WriteLine("Ответ сервера: {0}", request);
         }
+        while (true);
+    }
+
+    public static string SendRequestAndRecieveAnswer
+        (string server, int port, string message)
+    {
+        string request;
+        using var client = new TcpClient(server, port);
+        using Stream stream = client.GetStream();
+        using var bw = new BinaryWriter(stream);
+        bw.Write(message);
+        using BinaryReader br = new BinaryReader(stream);
+        request = br.ReadString();
+        return request;
     }
 }
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
 
-namespace Server
+class MyTcpListener
 {
-    class MainClass
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        TcpListener server = new TcpListener(IPAddress.Any, 13000);
+        server.Start();
+        while (true)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine("Жду подключения...");
+                using TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Подключение состоялось!");
+
+                using Stream stream = client.GetStream();
+                using var br = new BinaryReader(stream);
+                string data = br.ReadString();
+                Console.WriteLine("Получено: {0}", data);
+                data = ProcessCommand(data);
+                using var bw = new BinaryWriter(stream);
+                bw.Write(data);
+                Console.WriteLine("Отправлено: {0}", data);
+            }
+            catch
+            { }
         }
     }
+    public static string ProcessCommand(string data)
+    {
+        return data.ToUpper();
+    }
 }
+
